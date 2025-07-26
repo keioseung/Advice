83f36eea9bea45381f3fa4e12c479b5b0e9439fa
keioseung <keioseung@gmail.com>
2025-07-26 09:33:23 +0900

    fix: resolve UUID type casting issues in RLS policies and update backend models

--- disable_rls_temporarily.sql
+++ disable_rls_temporarily.sql
@@ -1,7 +1,7 @@
 -- Temporarily disable RLS for testing
 ALTER TABLE users DISABLE ROW LEVEL SECURITY;
 ALTER TABLE advices DISABLE ROW LEVEL SECURITY;
-
+ 
 -- Grant all permissions to anon role
 GRANT ALL ON users TO anon;
 GRANT ALL ON advices TO anon;
